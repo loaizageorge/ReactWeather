f5ef7f006f8f177cbb2532771fe218f5f9fd5382
George <loaizageorge@gmail.com>
2016-09-10 12:19:10 -0400

    Add custom css styles

--- app/app.jsx
+++ app/app.jsx
@@ -14,6 +14,9 @@ var WeatherMessage = require('WeatherMessage');
 require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
+//App css
+require('style!css!applicationStyles')
+
 ReactDOM.render(
 <Router history={hashHistroy}>
   <Route path="/" component={Main}>
